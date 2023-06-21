3303d0d40407b37f1614134d6da30c6ef55fc907
Shane Yokota <shaneyok@usc.edu>
2023-06-21 11:21:46 +0900

    mobile responsive edit

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -15,7 +15,7 @@ export default function Aboutme(){
                     <h1 className="aboutmetext">About Me</h1>
                     <img src={Shaneimg} alt="Shane_Image" className="shaneimg"></img>
                 </div>
-                <div style={{width: '45vw'}}>
+                <div className="abtmedescparent">
                     <p className="aboutmedescription">I'm Shane. Currently a Sophomore at the University of Southern California studying Computer Science. <br></br><br></br>My interests include software design and development, web development, artificial intelligence, and machine learning.
                     <br></br><br></br>
                     I'm passionate about problem-solving and coding, always up for the adventure of tackling complex issues and exploring new technologies. With my adaptability and drive, I confidently take on diverse projects, making meaningful contributions along the way.</p>
