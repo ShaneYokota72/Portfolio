d16a67cd6d6fb820639aa5a599089129e05e1bb8
Shane Yokota <shaneyok@usc.edu>
2023-12-26 21:41:44 -0800

    linkedin logo import issue fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import FooterLink from './FooterLink'
 export default function Footer() {
   return (
     <div className='flex flex-col sm:flex-row space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 w-full'>
-        <FooterLink href="https://www.linkedin.com/in/shaneyok/" iconlink="/linkedin_logo.svg" title="@shaneyok"/>
+        <FooterLink href="https://www.linkedin.com/in/shaneyok/" iconlink="/LinkedinIcon.svg" title="@shaneyok"/>
         <FooterLink href="https://github.com/ShaneYokota72" iconlink="/github_logo.svg" title="@ShaneYokota72"/>
     </div>
   )
