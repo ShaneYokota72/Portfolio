5299ff7eecbe0a3e418b622901f68dc4dee63341
Shane Yokota <shaneyok@usc.edu>
2024-10-05 03:52:12 -0700

    Portfolio UI edit and info update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,188 +6,194 @@ import { projectArray } from '@/util/projectData'
 import Project from '@/components/Project'
 import { ArrowDown } from 'lucide-react';
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className='flex flex-col gap-8'>
-      <div>
-        <div className='flex-1 flex items-end lg:items-start justify-between gap-2 lg:gap-8 '>
-          <div>
-            <h1 className='mb-4 text-5xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none'>hey, I'm Shane 👋</h1>
-            <h2 className='text-xl'>
-              Software Engineer and Entrepreneur. Check out what I've built {" "}
-              <Link href="#projects" className='underline items-center inline-flex gap-1'>
-                <span>projects</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-arrow-up-right mt-1" viewBox="0 0 16 16">
-                  <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
-                </svg>
-              </Link>
-            </h2>
-          </div>
-          <Image src='/ShaneImage.webp' height={160} width={160} alt="Profile Picture" className='w-40 h-40 aspect-square object-fill rounded-full'/>
-        </div>
-      </div>
-
-      <Sections title='About'>
-        <p className='text-light-grey dark:text-extra-light-grey'>
-          I'm a Junior at the University of Southern California majoring in Computer Science✌️.
-          
-          I love making things that make our life easier with my software engineering skills.   
-                 
-          In my free time, I enjoy visiting new places, attending hackathons, and hiking.
-        </p>
-      </Sections>
-      
-      <Sections title='Work Experience'>
-        <div className='flex flex-col gap-3'>
-          <WorkExperience 
-            imgSrc="/USCLogo.png" 
-            companyName="University of Southern California" 
-            position="Undergraduate Researcher(Network Reconnaissance Lab)" 
-            details={
-              <p>
-                Researching in Network Reconnaissance Lab under Dr. Corey Baker
-                <br/>
-                Some work can be found {" "}
-                <Link href="https://github.com/netreconlab" className='underline items-center inline-flex'>
-                  <span>here</span>
+    <>
+      <Head>
+        <title>Shane Yokota</title>
+      </Head>
+      <div className='flex flex-col gap-8'>
+        <div>
+          <div className='flex-1 flex items-end lg:items-start justify-between gap-2 lg:gap-8 '>
+            <div>
+              <h1 className='mb-4 text-5xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none'>hey, I'm Shane 👋</h1>
+              <h2 className='text-xl'>
+                Software Engineer and Entrepreneur. Check out what I've built {" "}
+                <Link href="#projects" className='underline items-center inline-flex gap-1'>
+                  <span>projects</span>
                   <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-arrow-up-right mt-1" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
                   </svg>
                 </Link>
-              </p>
-            }
-            startDate="Sep 2024" 
-            endDate="Present"
-          />
-          <WorkExperience 
-            imgSrc="/USCLogo.png" 
-            companyName="University of Southern California" 
-            position="Undergraduate Teaching Assistant(CS 356 - Computer Systems)" 
-            details={
-              <p>
-                Course Producer for CSCI 360: Computer Systems
-                <br/>
-                - Hosting weekly office hours to support students in Computer Systems fundamentals.
-              </p>
-            }
-            startDate="Aug 2024" 
-            endDate="Present"
-          />
-          <WorkExperience 
-            imgSrc="/KeepsakeIcon.webp" 
-            companyName="Keepsake" 
-            position="Software Developer"
-            details={
-              <p>
-                Architecting the core technology behind Keepsake, a modern estate planning platform that redefines the experience for attorneys and their clients, using Next.js/TypeScript and MongoDB. 
-                <br/>
-                <br/>
-                Contributing to new feature development like user dashboard, account sharing, and more, while collaborating with a team of 4 engineers to continuously deliver updates, bug fixes, and enhancements.
-              </p>
-            }
-            startDate="Sep 2023" 
-            endDate="Sep 2024"
-          />
-          <WorkExperience 
-            imgSrc="/EzreadLogo.jpeg" 
-            companyName="EZread" 
-            position="Software Engineer Intern" 
-            details={
-              <p>
-                Overhauled the EZread web application - a student-driven platform providing concise summaries of popular textbooks. Migrated the legacy React frontend to Next.js and MUI, creating comprehensive documentation to streamline future onboarding. Also, rebuilt and updated the backend with Node.js, Express.js, and MongoDB to securely store user credentials and textbook summaries.
-                <br/>
-                <br/>
-                Additionally, spearheaded SEO efforts, achieving a perfect 100 Lighthouse score by implementing server-side rendering and custom lazy image loading to optimize page load times for content-heavy pages. 
-                <br/>
-                <br/>
-                Separately, designed and developed the pricing page UI to drive higher conversion rates for the platform.
-              </p>
-            }
-            startDate="Jul 2023" 
-            endDate="Oct 2023"
-          />
-          <WorkExperience 
-            imgSrc="/DreamCoatSoftwareLogo.png" 
-            companyName="DreamCoat Software" 
-            position="Software Engineer Intern" 
-            details={
-              <p>
-                Contributed to the client onboarding process for DreamCoat's product placement technology that dynamically inserts branded products into films and shows based on the viewer. Developed the client signup portal and admin dashboard to view signup information and other vital metrics using React for the frontend and Spring Boot for the backend.
-                <br/>
-                <br/>
-                Furthermore, optimized data handling and state management, reducing page re-rendering by 84% for a seamless user experience.
-              </p>
-            }
-            startDate="Mar 2023" 
-            endDate="Jul 2023"
-          />
-          <WorkExperience 
-            imgSrc="/BluePantherMediaLogo.jpeg" 
-            companyName="Blue Panther Media" 
-            position="Software Engineer Intern" 
-            details={
-              <p>
-                Took a leading role in developing the content distribution website for Blue Panther Media, a prominent media company. Collaborated with two other engineers to design and implement a platform that streamlined the delivery of media content to consumers.
-                <br/>
-                <br/>
-                Specifically, played a pivotal role in architecting and executing the project, facilitating efficient media distribution for the company.
-              </p>
-            }
-            startDate="Jan 2023" 
-            endDate="Jul 2023"
-          />
-        </div>
-      </Sections>
-
-      <Sections title='Education'>
-        <div className='flex flex-col gap-3'>
-          <Education 
-            imgSrc="/USCLogo.png" 
-            companyName="University of Southern California" 
-            details="Bachelor of Science in Computer Science" 
-            startDate="Aug 2022" 
-            endDate="May 2026"
-            redirectLink="https://www.cs.usc.edu/"
-          />
-          <Education 
-            imgSrc="/BuildspaceLogo.jpg" 
-            companyName="Buildspace" 
-            details="s5" 
-            startDate="Jun 2024" 
-            endDate="Aug 2024"
-            redirectLink="https://buildspace.so/"
-          />
-        </div>
-      </Sections>
-
-      <Sections>
-        <div className='flex flex-col items-center justify-center mt-4' id='projects'>
-            <h5 className='text-4xl text-center font-bold mb-2'>Check out my recent projects!</h5>
-            <p className='text-center text-light-grey dark:text-extra-light-grey'>Here are some of my recent projects!</p>
-            <div className='flex flex-wrap gap-4 mt-4'>
-              {
-                projectArray.slice(0,4).map((project, i) => (
-                  <Project
-                    key={i}
-                    name={project.name}
-                    description={project.description}
-                    image={project.image}
-                    links={project.links}
-                  />
-                ))
-              }
+              </h2>
             </div>
+            <Image src='/ShaneImage.webp' height={160} width={160} alt="Profile Picture" className='w-40 h-40 aspect-square object-fill rounded-full'/>
+          </div>
         </div>
-      </Sections>
-      
-      <Sections>
-        <div className='flex flex-col items-center justify-center mt-4' id='projects'>
-            <h5 className='text-4xl text-center font-bold'>Get in Touch</h5>
-            <p className='text-center text-light-grey dark:text-extra-light-grey my-2'>Want to chat? Feel free to reach out to me via these</p>
-            <ArrowDown size={24} className='animate-bounce mt-2'/>
-        </div>
-      </Sections>
-    </div>
+
+        <Sections title='About'>
+          <p className='text-light-grey dark:text-extra-light-grey'>
+            I'm a Junior at the University of Southern California majoring in Computer Science✌️.
+            
+            I love making things that make our life easier with my software engineering skills.   
+                  
+            In my free time, I enjoy visiting new places, attending hackathons, and hiking.
+          </p>
+        </Sections>
+        
+        <Sections title='Work Experience'>
+          <div className='flex flex-col gap-3'>
+            <WorkExperience 
+              imgSrc="/USCLogo.png" 
+              companyName="University of Southern California" 
+              position="Undergraduate Researcher(Network Reconnaissance Lab)" 
+              details={
+                <p>
+                  Researching in Network Reconnaissance Lab under Dr. Corey Baker
+                  <br/>
+                  Some work can be found {" "}
+                  <Link href="https://github.com/netreconlab" className='underline items-center inline-flex'>
+                    <span>here</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-arrow-up-right mt-1" viewBox="0 0 16 16">
+                      <path fillRule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
+                    </svg>
+                  </Link>
+                </p>
+              }
+              startDate="Sep 2024" 
+              endDate="Present"
+            />
+            <WorkExperience 
+              imgSrc="/USCLogo.png" 
+              companyName="University of Southern California" 
+              position="Undergraduate Teaching Assistant(CS 356 - Computer Systems)" 
+              details={
+                <p>
+                  Course Producer for CSCI 360: Computer Systems
+                  <br/>
+                  - Hosting weekly office hours to support students in Computer Systems fundamentals.
+                </p>
+              }
+              startDate="Aug 2024" 
+              endDate="Present"
+            />
+            <WorkExperience 
+              imgSrc="/KeepsakeIcon.webp" 
+              companyName="Keepsake" 
+              position="Software Developer"
+              details={
+                <p>
+                  Architecting the core technology behind Keepsake, a modern estate planning platform that redefines the experience for attorneys and their clients, using Next.js/TypeScript and MongoDB. 
+                  <br/>
+                  <br/>
+                  Contributing to new feature development like user dashboard, account sharing, and more, while collaborating with a team of 4 engineers to continuously deliver updates, bug fixes, and enhancements.
+                </p>
+              }
+              startDate="Sep 2023" 
+              endDate="Sep 2024"
+            />
+            <WorkExperience 
+              imgSrc="/EzreadLogo.jpeg" 
+              companyName="EZread" 
+              position="Software Engineer Intern" 
+              details={
+                <p>
+                  Overhauled the EZread web application - a student-driven platform providing concise summaries of popular textbooks. Migrated the legacy React frontend to Next.js and MUI, creating comprehensive documentation to streamline future onboarding. Also, rebuilt and updated the backend with Node.js, Express.js, and MongoDB to securely store user credentials and textbook summaries.
+                  <br/>
+                  <br/>
+                  Additionally, spearheaded SEO efforts, achieving a perfect 100 Lighthouse score by implementing server-side rendering and custom lazy image loading to optimize page load times for content-heavy pages. 
+                  <br/>
+                  <br/>
+                  Separately, designed and developed the pricing page UI to drive higher conversion rates for the platform.
+                </p>
+              }
+              startDate="Jul 2023" 
+              endDate="Oct 2023"
+            />
+            <WorkExperience 
+              imgSrc="/DreamCoatSoftwareLogo.png" 
+              companyName="DreamCoat Software" 
+              position="Software Engineer Intern" 
+              details={
+                <p>
+                  Contributed to the client onboarding process for DreamCoat's product placement technology that dynamically inserts branded products into films and shows based on the viewer. Developed the client signup portal and admin dashboard to view signup information and other vital metrics using React for the frontend and Spring Boot for the backend.
+                  <br/>
+                  <br/>
+                  Furthermore, optimized data handling and state management, reducing page re-rendering by 84% for a seamless user experience.
+                </p>
+              }
+              startDate="Mar 2023" 
+              endDate="Jul 2023"
+            />
+            <WorkExperience 
+              imgSrc="/BluePantherMediaLogo.jpeg" 
+              companyName="Blue Panther Media" 
+              position="Software Engineer Intern" 
+              details={
+                <p>
+                  Took a leading role in developing the content distribution website for Blue Panther Media, a prominent media company. Collaborated with two other engineers to design and implement a platform that streamlined the delivery of media content to consumers.
+                  <br/>
+                  <br/>
+                  Specifically, played a pivotal role in architecting and executing the project, facilitating efficient media distribution for the company.
+                </p>
+              }
+              startDate="Jan 2023" 
+              endDate="Jul 2023"
+            />
+          </div>
+        </Sections>
+
+        <Sections title='Education'>
+          <div className='flex flex-col gap-3'>
+            <Education 
+              imgSrc="/USCLogo.png" 
+              companyName="University of Southern California" 
+              details="Bachelor of Science in Computer Science" 
+              startDate="Aug 2022" 
+              endDate="May 2026"
+              redirectLink="https://www.cs.usc.edu/"
+            />
+            <Education 
+              imgSrc="/BuildspaceLogo.jpg" 
+              companyName="Buildspace" 
+              details="s5" 
+              startDate="Jun 2024" 
+              endDate="Aug 2024"
+              redirectLink="https://buildspace.so/"
+            />
+          </div>
+        </Sections>
+
+        <Sections>
+          <div className='flex flex-col items-center justify-center mt-4' id='projects'>
+              <h5 className='text-4xl text-center font-bold mb-2'>Check out my recent projects!</h5>
+              <p className='text-center text-light-grey dark:text-extra-light-grey'>Here are some of my recent projects!</p>
+              <div className='flex flex-wrap gap-4 mt-4'>
+                {
+                  projectArray.slice(0,4).map((project, i) => (
+                    <Project
+                      key={i}
+                      name={project.name}
+                      description={project.description}
+                      image={project.image}
+                      links={project.links}
+                    />
+                  ))
+                }
+              </div>
+          </div>
+        </Sections>
+        
+        <Sections>
+          <div className='flex flex-col items-center justify-center mt-4' id='projects'>
+              <h5 className='text-4xl text-center font-bold'>Get in Touch</h5>
+              <p className='text-center text-light-grey dark:text-extra-light-grey my-2'>Want to chat? Feel free to reach out to me via these</p>
+              <ArrowDown size={24} className='animate-bounce mt-2'/>
+          </div>
+        </Sections>
+      </div>
+    </>
   )
 }
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,6 +3,26 @@ import React from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import Tag from './Tag'
+import clsx from 'clsx'
+
+const HIGH_LIGHT_COLORS:Record<string, string> = {
+    'Tacer': 'via-purple-800',
+    'Tinker': 'via-green-800',
+    'Med2Meals': 'via-rose-800',
+    'DevMesh': 'via-yellow-800',
+}
+const HIGH_LIGHT_BOTTOM_MAIN_COLORS:Record<string, string> = {
+    'Tacer': 'via-purple-800',
+    'Tinker': 'via-green-800',
+    'Med2Meals': 'via-rose-800',
+    'DevMesh': 'via-yellow-800',
+}
+const HIGH_LIGHT_BOTTOM_SUB_COLORS:Record<string, string> = {
+    'Tacer': 'group-hover:via-purple-500',
+    'Tinker': 'group-hover:via-green-500',
+    'Med2Meals': 'group-hover:via-rose-500',
+    'DevMesh': 'group-hover:via-yellow-500',
+}
 
 interface ProjectProps {
     name: string
@@ -15,11 +35,11 @@ export default function Project({
     name,
     description,
     image,
-    links
+    links,
 }: ProjectProps) {
     const router = useRouter()
     return (
-        <div className='w-full sm:w-[calc(50%-16px)] h-fit sm:h-[315px] flex-col gap-2 rounded-lg border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 overflow-hidden m-1'>
+        <div className='relative w-full sm:w-[calc(50%-16px)] h-fit sm:h-[320px] flex-col gap-2 rounded-lg border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 overflow-hidden m-1 group hover:brightness-105'>
             <Image src={image} width={440} height={280} alt={name} className='object-fill w-full aspect-video h-44'/>
             <div className='sm:h-[139px] flex flex-col gap-4 sm:gap-0 sm:justify-between p-2'>
                 <div>
@@ -36,6 +56,9 @@ export default function Project({
                     }
                 </div>
             </div>
+
+            <div className={clsx("h-2 w-full bg-gradient-to-l from-transparent to-transparent group-hover:blur-xl blur-3xl m-auto rounded transition-all absolute bottom-0", HIGH_LIGHT_COLORS[name])}></div>
+            <div className={clsx('w-[70%] h-0.5 left-0 right-0 mx-auto group-hover:w-full bg-gradient-to-l from-transparent to-transparent rounded transition-all absolute bottom-0', HIGH_LIGHT_BOTTOM_MAIN_COLORS[name], HIGH_LIGHT_BOTTOM_SUB_COLORS[name])}/>
         </div>
     )
 }
