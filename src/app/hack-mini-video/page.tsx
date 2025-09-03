59c6db4a9ec9f62e2d253f722cf8640efe6c75fd
Shane Yokota <shaneyok@usc.edu>
2025-09-03 16:41:21 -0700

    update: work info updated

--- src/app/hack-mini-video/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function page() {
-  return (
-    <div>
-      <a href="https://www.youtube.com/watch?v=nSll1aaj5ac" target="_blank">https://www.youtube.com/watch?v=nSll1aaj5ac</a>
-    </div>
-  )
-}
